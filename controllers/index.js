905590bc63988263bcd1aaa7648fe84851a87fad
rajesh23-drg <rmanjunath@dresourcesgroup.com>
2017-03-22 13:36:29 +0530

    sample usage

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Todo.findAll().then(function(data){
+    res.render('index', { title: 'Express', data: data });
+    console.log(data);
+  });
+  // res.render('index', { title: 'Express', data: data });
 });
 
 module.exports = router;
