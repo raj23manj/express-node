95102a8bf5f7902b17fbe8d055ed11517c9e0894
rajesh23-drg <rmanjunath@teamdrg.com>
2017-10-13 15:39:21 +0530

    removed bower

--- app.js
+++ app.js
@@ -29,8 +29,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
+
+app.use('/components',  express.static(__dirname + '/node_modules'));
 
 app.use('/', index);
 app.use('/users', users);
