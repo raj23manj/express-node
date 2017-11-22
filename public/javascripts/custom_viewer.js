5ba3d1d560f10428eb8a78c6bcf9c9f20b5507a8
rajesh23-drg <rmanjunath@teamdrg.com>
2017-11-22 15:15:27 +0530

    disable save

--- public/javascripts/custom_viewer.js
+++ public/javascripts/custom_viewer.js
@@ -5,3 +5,26 @@ $(function(){
   $('#viewBookmark' ).remove();
   $('#secondaryToolbarToggle' ).remove();
 });
+
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    e.preventDefault();
+  }
+});
+
+
+function click (e) {
+  if (!e)
+    e = window.event;
+  if ((e.type && e.type == "contextmenu") || (e.button && e.button == 2) || (e.which && e.which == 3)) {
+    if (window.opera)
+      window.alert("");
+    return false;
+  }
+}
+
+if (document.layers)
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown = click;
+document.oncontextmenu = click;
