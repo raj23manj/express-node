a3a1d553dd9cb4f3364c3744625db43670763646
Ghosh <saghosh@BAN15045.local>
2017-11-28 13:33:58 +0530

    added new model

--- controllers/registrations.js
+++ controllers/registrations.js
@@ -4,35 +4,70 @@ const router = express.Router();
 const models = require('../server/models/');
 const async = require('async');
 const path = require('path');
+const LocalStrategy = require('passport-local').Strategy;
+const pry = require('pryjs')
 
-/* GET registrations page. */
-router.get('/', function(req, res, next) {
-  models.Todo.findAll().then(function(data){
-    res.render('index', { title: 'Express', data: data });
-  });
-});
 
 router.get('/signup', function(req, res) {
   res.render('registrations/signup', { });
 });
 
-router.post('/register', function(req, res) {
-  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-    if (err) {
-      return res.render('register', { account : account });
-    }
-    passport.authenticate('local')(req, res, function () {
-      res.redirect('/');
-    });
+router.post('/register_submit', function(req, res) {
+
+  var user = models.User.build({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: models.User.generateHash(req.body.password)
   });
+
+  user.validate().then(function(errors) {
+    if(errors)
+    {
+      //new_or_edit(req, res, errors["errors"]);
+    }else {
+      user.save().then(function() {
+        req.flash('info', 'User register Successfully !');
+        res.redirect('/books');
+      });
+    }
+  });
+
 });
 
 router.get('/login', function(req, res) {
-  res.render('login', { user : req.user });
+  res.render('registrations/login', { });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/');
+router.post('/login_post', function(req, res, next) {
+  // ask passport to authenticate
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      // if error happens
+      return next(err);
+    }
+
+    if (!user) {
+      // if authentication fail, get the error message that we set
+      // from previous (info.message) step, assign it into to
+      // req.session and redirect to the login page again to display
+      req.session.messages = info.message;
+      return res.redirect('/registrations/login');
+    }
+
+    // if everything's OK
+    req.logIn(user, function(err) {
+      if (err) {
+        req.session.messages = "Error";
+        return next(err);
+      }
+
+      // set the message
+      req.session.messages = "Login successfully";
+      return res.redirect('/books');
+    });
+
+  })(req, res, next);
 });
 
 router.get('/logout', function(req, res) {
@@ -40,9 +75,23 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-router.get('/ping', function(req, res){
-  res.status(200).send("pong!");
-});
+passport.use(new LocalStrategy(function(email, password, done){
+  // done is a calback
+  models.user.getUserByEmail(email, function(err, user){
+    if(err) throw err;
+    if(!user){
+      return done(null, false, {message: 'unknown User'});
+    }
 
+    models.user.comparePassword(password, user.password, function(err, isMatch){
+      if(err) return done(err);
+      if(isMatch){
+        return done(null, user);
+      }else{
+        return done(null, false, {message: 'Invalid Password'});
+      }
+    })
+  });
+}));
 
 module.exports = router;
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+var bcrypt   = require('bcrypt-nodejs');
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     first_name: DataTypes.STRING,
@@ -6,12 +7,21 @@ module.exports = function(sequelize, DataTypes) {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     salt: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+  },
+    {
+      classMethods: {
+        associate: function(models) {
+          // associations can be defined here
+        },
+        generateHash : function(password) {
+          return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+        }
+      },
+      instanceMethods: {
+        validPassword : function(password) {
+          return bcrypt.compareSync(password, this.localpassword);
+        }
       }
-    }
   });
   return User;
 };
